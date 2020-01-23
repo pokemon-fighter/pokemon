35b80651eccd3d90c8b026b81c404f5d24d1b664
Okka Linardi <okka.linardi@gmail.com>
2020-01-23 20:11:32 +0700

    Done Client for landing page and character selection

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <b-row align-v="center" align-h="center" class="home">
+    <b-button variant="light" id="startButton"><img src="../../public/startButton.png"></b-button>
+  </b-row>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  name: 'home'
 }
 </script>
+
+<style scoped>
+.home {
+  background-image: url('../../public/landing.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 100vh;
+}
+#startButton {
+  margin-top: 7%;
+  border-radius: 50%;
+  height: 30vh;
+  width: 17vw;
+}
+#startButton img {
+  height: 30vh;
+  width: 15vw;
+  object-fit: contain;
+}
+</style>
