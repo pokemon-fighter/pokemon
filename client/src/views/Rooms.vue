983dcc83311872f2f2623b10f591b7a0042808fb
Richard Khonan <richardkhonan0797@gmail.com>
2020-01-24 10:32:07 +0700

    Set room creator as player1 and user who joined the room as player2

--- client/src/views/Rooms.vue
+++ client/src/views/Rooms.vue
@@ -1,16 +1,17 @@
 <template>
   <div>
     <b-container fluid class="rooms">
-        <form @submit.prevent="addNewRoom" class="text-center">
+        <form class="text-center">
             <input class="mr-1" v-model="roomName" type="text" placeholder="Input room name">
             <input class="ml-1 mr-1" v-model="roomCode" type="number" placeholder="Input room code(number)">
-            <b-button variant="primary">Submit</b-button>
+            <b-button variant="primary" @click.prevent="addNewRoom">Submit</b-button>
         </form>
         <b-card-group columns class="mt-5 flex">
             <div v-for="room in rooms" :key="room.id">
                 <b-card bg-variant="light" :header="room.roomName" class="text-center mt-3">
-                    <b-card-text class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
-                    <b-button variant="primary">Join</b-button>
+                    <b-card-text v-if="typeof room.player1 != 'undefined'">1. {{room.player1.username}}</b-card-text>
+                    <b-card-text v-if="typeof room.player2 != 'undefined'">2. {{room.player2.username}}</b-card-text>
+                    <b-button variant="primary" @click.prevent="joinThisRoom(room.id)">Join</b-button>
                 </b-card>
             </div>
         </b-card-group>
@@ -32,7 +33,7 @@ export default {
       ...mapState(['rooms'])
   },
   methods: {
-    ...mapActions(['addRoom']),
+    ...mapActions(['addRoom','joinRoom']),
     addNewRoom() {
       this.addRoom({
         roomName: this.roomName,
@@ -40,6 +41,9 @@ export default {
       })
       this.roomName = '',
       this.roomCode = ''
+    },
+    joinThisRoom(id) {
+        this.joinRoom(id)
     }
   }
 }
